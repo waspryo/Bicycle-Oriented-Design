bd5e4b8c66764b71232bac1e9daee27935bf0ac6
waspryo <sugimoto@liddell.tokyo>
2020-02-15 16:55:06 +0900

    Structとcollect

--- ruby.rb
+++ ruby.rb
@@ -72,3 +72,80 @@ end
 def cog
   @cog * (foo ? bar_adjusutment : baz_adjustment)
 end
+
+------------------------------------------------------------------------------------------------------------
+class ObscuringReferences
+attr_writer :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def diameters
+    # 0はリム、１はタイヤ
+    data.collect {| cell |}
+      cell[0] + (cell[1] * 2)
+  end
+end
+
+# リムとタイヤサイズ(ここではミリメートル)の２次元配列
+@data = [[633, 20], [622, 43], [622, 34], [623,43]]
+------------------------------------------------------------------------------------------------------------
+class RevealingReferences
+  attr_reader :wheels
+
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect { |wheel|
+      wheel.rim + (wheel.tire * 2) }
+  end
+  #  これだけでもwheelにrim/tireを送れる
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect { |cell|
+    Wheel.new(cell[0], cell[1]) }
+  end
+end
+------------------------------------------------------------------------------------------------------------
+# メソッドの単一の責任にする
+def diameters
+  wheels.collect { |wheel| diameter(wheel) }
+end
+
+# 次に ^ 「1」の車輪の直径を計算する
+def diameter(wheel)
+  wheel.rim + (wheel.tire * 2)
+end
+
+array = [1,2,3,4,5,6,7,8]
+
+p array.collect { |sum| sum += 3 }
+
+array = [1,2,3,4,5,6,7,8]
+p array.inject(0){ |res, x| res += x; res }
+
+pet = Struct.new(:name, :age)
+# dogにポチと7のパラメーターを持たせる
+dog = pet.new('ポチ', 7)
+# .nameでdogにアクセスする
+p dog.name
+------------------------------------------------------------------------------------------------------------
+# Structを生成
+record = Struct.new(:english, :math, :history) do
+  # resultメソッド
+  # 各科目の成績を出力する
+
+  def result
+    printf "English: %s Math: %s History: %s\n",english,math,history
+  end
+
+  # meanメソッド
+  # 3科目の平均を得る
+  def mean
+    (english + math + history) / 3.0
+  end
+end
