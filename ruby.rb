e96688110a00872747933bbcf963bdac1964d815
waspryo <sugimoto@liddell.tokyo>
2020-02-15 15:27:02 +0900

    インスタンスの隠蔽

--- ruby.rb
+++ ruby.rb
@@ -10,7 +10,47 @@ puts ratio
 
 
 class Gear
-  attr_reader :chairning, :cog
+  attr_reader :chairning, :cog, :rim, :tire
+
+  def initialize(chairning, cog, rim, tire)
+    @chairning = chairning
+    @cog       = cog
+    @rim       = rim
+    @tire       = tire
+  end
+
+  def ratio
+    chairning / cog.to_f
+  end
+
+  def gear_inches
+    #タイヤはリムの周りを囲むので、直径を計算するためには2倍にする
+    ratio * (rim + (tire * 2))
+  end
+end
+------------------------------------------------------------------------------------------------------------
+class Gear
+
+  def initialize(chairning, cog)
+    @chairning = chairning
+    @cog       = cog <--- #破滅への道
+  end
+
+  def ratio
+    chairning / cog.to_f
+  end
+
+  def gear_inches
+    #タイヤはリムの周りを囲むので、直径を計算するためには2倍にする
+    ratio * (rim + (tire * 2))
+  end
+end
+puts Gear.new(53, 11).ratio
+puts Gear.new(32, 21).ratio
+------------------------------------------------------------------------------------------------------------
+
+class Gear
+  attr_reader :chairning, :cog #<--------
 
   def initialize(chairning, cog)
     @chairning = chairning
@@ -18,8 +58,17 @@ class Gear
   end
 
   def ratio
-    chairning / cog.to_f
+    chairning / cog.to_f #<--------------
   end
 end
 
-puts Gear.new(53, 11).ratio
+------------------------------------------------------------------------------------------------------------
+# シンプルな再実装
+def cog
+  @cog * unanticipated_adjustment_factor
+end
+
+# より複雑な実装
+def cog
+  @cog * (foo ? bar_adjusutment : baz_adjustment)
+end
