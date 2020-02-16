e3759bc27b93417c3fb5b13b9d205b6e06c262b8
waspryo <sugimoto@liddell.tokyo>
2020-02-16 15:29:22 +0900

    メソッド切り分け

--- ruby.rb
+++ ruby.rb
@@ -275,3 +275,41 @@ class Gear
   end
 end
 
+------------------------------------------------------------------------------------------------------------
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
+  end
+
+end
+------------------------------------------------------------------------------------------------------------
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def ratio
+    chairning / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+end
