e5629d943f3f9bdbf5ddb2771baefcdb6758e75e
waspryo <sugimoto@liddell.tokyo>
2020-02-29 13:17:48 +0900

    even?

--- ruby.rb
+++ ruby.rb
@@ -546,3 +546,77 @@ elsif prepare.kind_of?(Driver)
   prepare.gas_up(vechicle)
   prepare.fill_water_tank(vechicle)
 end
+------------------------------------------------------------------------------------------------------------
+class Bicycle
+  attr_reader :size, :tape_color
+
+  def initialize(args)
+    @size       = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  def spares
+    {
+     chain:       '10-speed',
+     tire_size:   '23',
+     tape_color:  'tape_color'
+    }
+  end
+end
+------------------------------------------------------------------------------------------------------------
+class Bicycle
+  attr_reader :style, :size, :tape_color, :front_sock, :rear_shock
+
+  def initialize(args)
+    @style      = args[:style]
+    @size       = args[:size]
+    @tape_color = args[:tape_color]
+    @front_sock = args[:front_sock]
+    @rear_shock = args[:rear_shock]
+  end
+
+  def spares
+    if style == :road
+      {
+        chain:      '10-speed',
+        tire_size:  '23', # millimeters
+        tape_color: 'tape_color',
+      }
+    else
+      {
+        chain:       '10-speed',
+        tire_size:   '2.1', # inches
+        rear_shock:  rear_shock,
+      }
+  end
+end
+------------------------------------------------------------------------------------------------------------
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+
+end
+------------------------------------------------------------------------------------------------------------
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum += n
+end
+sum
+
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n * 10 : n
+
+  sum += sum_value
+end
