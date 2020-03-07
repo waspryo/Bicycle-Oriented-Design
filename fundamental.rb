0867db0ecb67479e430980342286b4f4fa455a21
waspryo <sugimoto@liddell.tokyo>
2020-03-07 18:14:25 +0900

    json

--- fundamental.rb
+++ fundamental.rb
@@ -135,3 +135,73 @@ class Bicycle
   end # 共通の内容はスーパークラスに初期値としてもたせる
 end
 ------------------------------------------------------------------------------------------------------------
+person = {name: 'ED', age: 23, friends: ['Mustang', 'Alphonce'], phones: [home: '070-1234-5678', mobile: 0124-765-2316]}
+person[:name] # => ED
+person[:firends] # => ["Mustang", "Alphonce"]
+person[:firends][0] # => "Mustang"
+
+i = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+]
+
+------------------------------------------------------------------------------------------------------------
+def buy_burger(menu, drink: true, poteto: true)
+
+  if menu
+
+  end
+
+  if drink
+
+  end
+
+  if poteto
+
+  end
+end
+
